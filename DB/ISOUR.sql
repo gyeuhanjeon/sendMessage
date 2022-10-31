41ded025051d0ae2150215e2102c06cd264253ea
JUNG830 <j_jung830@naver.com>
2022-10-31 19:47:06 +0900

    1031

---
+++ DB/ISOUR.sql
@@ -0,0 +1,34 @@
+CREATE TABLE I_MEMBER (
+    
+    ID      VARCHAR2(30) PRIMARY KEY,
+    PASSWORD     VARCHAR2(20),
+    NAME    VARCHAR2(30),
+    GENDER  VARCHAR2(10),
+    BIRTH   VARCHAR2(20),
+    REGION1  VARCHAR2(20),
+    REGION2  VARCHAR2(20)
+);
+
+INSERT INTO I_MEMBER VALUES('LEEMH', 'SPHB8250', '이민형', '1', '96/01/4', '', '');
+INSERT INTO I_MEMBER VALUES('JEONKH', '1234567', '전규한', '1', '96/01/4', '', '');
+INSERT INTO I_MEMBER VALUES('JOHK', '1234567', '조혜경', '1', '96/01/4', '', '');
+INSERT INTO I_MEMBER VALUES('WOOHJ', '1234567', '우혜정', '1', '96/01/4', '', '');
+INSERT INTO I_MEMBER VALUES('LEEDK', '12345612', '이동균', '1', '96/01/4', '', '');
+INSERT INTO I_MEMBER VALUES('admin', 'admin1234', '관리자', '1', '96/01/4', '', '');
+INSERT INTO I_MEMBER VALUES('newbi', 'newbi1234', '뉴비', '남', '96/01/4', '', '');
+INSERT INTO I_MEMBER VALUES('qweqwe', 'qweqwe111', '뉴비', '남', '96/01/4', '', '');
+
+INSERT INTO I_MEMBER(ID, PWD, NAME, GENDER, BIRTH, REGION) VALUES(?, ?, ?, ?, ?, ?);
+
+SELECT *
+FROM I_MEMBER;
+
+COMMIT;
+
+drop table i_member;
+
+SELECT * FROM I_MEMBER WHERE ID = 'admin';
+
+
+
+
