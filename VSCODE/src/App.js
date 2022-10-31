ce401ef0c6f0e3f1bbbf42dec6dff589e29f0420
JUNG830 <j_jung830@naver.com>
2022-10-31 19:45:28 +0900

    1031_회원가입완(모달제외)

--- VSCODE/src/App.js
+++ VSCODE/src/App.js
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './project/pages/Main';
 import Login from './project/Login/Login';
-// import SignUp from './project/SingUp/SignUp';
 import Home from './project/HOME/Home';
 import MyPage from './project/pages/MyPage';
 import MemberInfo from './project/pages/MemberInfo';
 import MemberDrop from './project/pages/MemberDrop';
-import SignUpTest from './project/SingUp/SignUpTest';
+import SignUp from './project/SingUp/SignUpTest';
 
 
 function App() {
@@ -15,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
-        <Route path='/signup' element={<SignUpTest />} />
+        <Route path='/signup' element={<SignUp />} />
         <Route path='/home' element={<Home />} />
         <Route path='/mypage' element={<MyPage />} />
         <Route path='/MemberInfo' element={<MemberInfo />} />
